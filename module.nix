42f353caaa010d0c8dcacabdb24b5d0fa48bcf22
nixpup <nixpup@noreply.codeberg.org>
2025-12-05 21:15:36 +0100

    Updated Nix Module

--- module.nix
+++ module.nix
@@ -5,10 +5,47 @@ with lib;
 let
   cfg = config.nixMacs;
   logoImage = "${./config/nix_emacs_logo_small.png}";
+  
   customEorg = pkgs.runCommand "e.org" {} ''
     substitute ${./config/e.org} $out \
       --replace "~/Pictures/nix_emacs_logo_small.png" "${logoImage}"
   '';
+  
+  # Create the configured Emacs with packages FIRST
+  configuredEmacs = pkgs.emacsWithPackages (epkgs: with epkgs; [
+    use-package
+    color-theme-sanityinc-tomorrow
+    company
+    emms
+    fancy-dabbrev
+    lsp-mode
+    lsp-ui
+    markdown-mode
+    multi-term
+    multiple-cursors
+    nix-buffer
+    nix-mode
+    rainbow-mode
+    rust-mode
+    rustic
+    wttrin
+    hydra
+    all-the-icons
+    haskell-mode
+    arduino-mode
+    flycheck
+    gruvbox-theme
+    bongo
+    impatient-mode
+    simple-httpd
+    org
+  ] ++ (cfg.extraPackages epkgs));
+  
+  # Then create wrapper that references it
+  nixmacs = pkgs.writeShellScriptBin cfg.binaryName ''
+    exec ${configuredEmacs}/bin/emacs "$@"
+  '';
+  
 in {
   options.nixMacs = {
     enable = mkEnableOption "custom Emacs configuration";
@@ -30,64 +67,41 @@ in {
       default = epkgs: with epkgs; [];
       description = "Extra Emacs packages to install";
     };
+
+    binaryName = mkOption {
+      type = types.str;
+      default = "nixmacs";
+      description = "Name of the Emacs binary command";
+    };
   };
 
   config = mkIf cfg.enable {
     home.sessionVariables = {
-      NIXMACS_LOGO_PATH = "${./config/nix_emacs_logo_small.png}";
+      NIXMACS_LOGO_PATH = logoImage;
     };
+    
     home.packages = [
-      (pkgs.emacsWithPackages (epkgs: with epkgs; [
-        # Your default packages here
-        use-package
-        color-theme-sanityinc-tomorrow
-        company
-        emms
-        fancy-dabbrev
-        lsp-mode
-        lsp-ui
-        markdown-mode
-        multi-term
-        multiple-cursors
-        nix-buffer
-        nix-mode
-        rainbow-mode
-        rust-mode
-        rustic
-        wttrin
-        hydra
-        all-the-icons
-        haskell-mode
-        arduino-mode
-        flycheck
-        gruvbox-theme
-        bongo
-        impatient-mode
-        simple-httpd
-        org  # Important: make sure org is included!
-        # ... add more packages
-      ] ++ (cfg.extraPackages epkgs)))
+      nixmacs  # Only install the wrapper, not both!
       pkgs.rust-analyzer
       pkgs.zathura
       pkgs.mpv
     ];
 
-    # Create the .emacs bootstrap file
     home.file.".emacs" = {
       text = ''
         (let ((orgfile "~/.e.org")
               (elfile "~/.e.el"))
           (when (or (not (file-exists-p elfile))
-	            (file-newer-than-file-p orgfile elfile))
+                    (file-newer-than-file-p orgfile elfile))
             (require 'org)
             (org-babel-tangle-file orgfile elfile))
           (load-file elfile))
       '';
     };
     
-    # Symlink your org-mode configuration
+    # Use the MODIFIED e.org with substituted path
     home.file.".e.org" = {
-      source = ./config/e.org;
+      source = customEorg;
     };
   };
 }
